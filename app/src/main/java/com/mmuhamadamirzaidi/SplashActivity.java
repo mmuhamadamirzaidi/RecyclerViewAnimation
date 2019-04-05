66c1265b2b5aba0682a6f3708c9bfa9716e4da80
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-04-05 17:37:40 +0800

    #5 Search Filter

---
+++ app/src/main/java/com/mmuhamadamirzaidi/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.mmuhamadamirzaidi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView SplashLogo;
+    private int SPLASH_TIME_OUT=1000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashlogo);
+
+        SplashLogo = (ImageView)findViewById(R.id.splash_logo_startup);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(splash);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.splashanimation);
+        SplashLogo.startAnimation(myanim);
+    }
+}
